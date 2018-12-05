8539f748030e8464c3c3f1c463ebf5ebc43d9b6d
Keenan <kpautler4@gmail.com>
2018-12-05 16:02:49 -0700

    Added a CORS header to allow my app to talk to the api

--- router.js
+++ router.js
@@ -10,6 +10,8 @@ router.param('userName', function (req, res, next, userName) {
 
 router.get('/users',  function (req, res) {
     userController.getAll().then(function (users) {
+		res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         res.send(users);
     }, function (error) {
         res.status(500).send({
@@ -21,6 +23,8 @@ router.get('/users',  function (req, res) {
 router.get('/users/:userName', function (req, res, userName){
 	userController.findOne(userToUpdate).then(function (user) {
 		console.log(user);	
+		res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 		res.send(user);
 	}), function (error) {
 		res.status(500).send({
